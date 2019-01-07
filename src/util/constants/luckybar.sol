18f157568a6bd1d7687e9a3c5189aeede4e4f63b
joshua <joshua.feryll@gmail.com>
2019-01-07 14:00:48 +0900

    init value change

--- src/util/constants/luckybar.sol
+++ src/util/constants/luckybar.sol
@@ -5,34 +5,10 @@ pragma solidity ^0.4.24;
   * @dev
   *
   */
-interface IERC20 {
-  function totalSupply() external view returns (uint256);
 
-  function balanceOf(address who) external view returns (uint256);
+import "./Toka_ERC827.sol";
 
-  function allowance(address owner, address spender)
-    external view returns (uint256);
 
-  function transfer(address to, uint256 value) external returns (bool);
-
-  function approve(address spender, uint256 value)
-    external returns (bool);
-
-  function transferFrom(address from, address to, uint256 value)
-    external returns (bool);
-
-  event Transfer(
-    address indexed from,
-    address indexed to,
-    uint256 value
-  );
-
-  event Approval(
-    address indexed owner,
-    address indexed spender,
-    uint256 value
-  );
-}
 /**
  * @title Ownership
  * @dev Ownership contract establishes ownership (via owner address) and provides basic authorization control
@@ -108,100 +84,136 @@ contract Bank is Ownership {
  */
 contract LuckyBar is Bank {
 
-    uint256 public minBetT2T;
-    uint256 public minBetE2T;
-    uint256 public minBetT2E;
-    uint256 public minBetE2E;
-    uint256 public houseEdgeT2T; // in %
-    uint256 public houseEdgeE2T; // in %
-    uint256 public houseEdgeT2E; // in %
-    uint256 public houseEdgeE2E; // in %
-    uint256 public E2TRatio;
+    struct pair {
+        uint256 minBet;
+        uint256 houseEdge; // in %
+        bool bEnabled;
+    }
+
+    pair public sE2E;
+    pair public sE2R;
+    pair public sR2E;
+    pair public sR2R;
+
+    uint256 public E2R_Ratio;
     uint256 private salt;
     IERC20 private token;
+    Rupy private rupy;
     address public manager;
 
     // Either True or False + amount
-    event Won(bool _status, uint _amount);
+    event Won(bool _status, string _rewardType, uint _amount);
+    event Swapped(string _target, uint _amount);
 
     // sets the stakes of the bet
     constructor() payable public {
         estalishOwnership();
-        setProperties("thisissaltIneedtomakearandomnumber", 10000);
+        setProperties("thisissaltIneedtomakearandomnumber", 100000);
         setToken(0x1794f29a384a8329da53e7749ee333af31c7bc36); // toka mainnet
+        setRupy(0x1794f29a384a8329da53e7749ee333af31c7bc36); // rupy mainnet
         setGameMinBet(100e18, 0.1 ether, 100e18, 0.1 ether);
-        setGameFee(1,1,5,5);
+        setGameFee(1,0,5,5);
+        EnableGame(true, true, false, true);
         manager = owner;
     }
 
-    function setGameFee(uint256 T2T, uint256 E2T, uint256 T2E, uint256 E2E) onlyOwner {
-        houseEdgeT2T = T2T;
-        houseEdgeE2T = E2T;
-        houseEdgeT2E = T2E;
-        houseEdgeE2E = E2E;
+    function EnableGame(bool R2R, bool E2R, bool R2E, bool E2E) onlyOwner {
+        sR2R.bEnabled = R2R;
+        sE2R.bEnabled = E2R;
+        sR2E.bEnabled = R2E;
+        sE2E.bEnabled = E2E;
     }
 
-    function setGameMinBet(uint256 T2T, uint256 E2T, uint256 T2E, uint256 E2E) onlyOwner {
-        minBetT2T = T2T;
-        minBetE2T = E2T;
-        minBetT2E = T2E;
-        minBetE2E = E2E;
+    function setGameFee(uint256 R2R, uint256 E2R, uint256 R2E, uint256 E2E) onlyOwner {
+        sR2R.houseEdge = R2R;
+        sE2R.houseEdge = E2R;
+        sR2E.houseEdge = R2E;
+        sE2E.houseEdge = E2E;
+    }
+
+    function setGameMinBet(uint256 R2R, uint256 E2R, uint256 R2E, uint256 E2E) onlyOwner {
+        sR2R.minBet = R2R;
+        sE2R.minBet = E2R;
+        sR2E.minBet = R2E;
+        sE2E.minBet = E2E;
     }
 
     function setToken(address _token) public onlyOwner {
         token = IERC20(_token);
     }
 
+    function setRupy(address _rupy) public onlyOwner {
+        rupy = Rupy(_rupy);
+    }
+
     function setManager(address _manager) public onlyOwner {
         manager = _manager;
     }
 
-    function setProperties(string _salt, uint _E2TRatio) public onlyOwner {
-        require(_E2TRatio > 0);
+    function setProperties(string _salt, uint _E2R_Ratio) public onlyOwner {
+        require(_E2R_Ratio > 0);
         salt = uint(keccak256(_salt));
-        E2TRatio = _E2TRatio;
+        E2R_Ratio = _E2R_Ratio;
     }
 
     function() public { //fallback
         revert();
     }
 
-    function playT2T(uint256 _value) payable public {
-        require(_value >= minBetT2T);
-        require(token.transferFrom(msg.sender, manager, _value));
+    function swapR2T(address _from, uint256 _value) payable public {
+        require(rupy.transferFrom(_from, manager, _value));
+        require(token.transferFrom(manager, _from, _value));
 
-        uint256 amountWon = _value * (50 + uint256(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - houseEdgeT2T) / 100;
-        require(token.transferFrom(manager, msg.sender, amountWon));
-
-        emit Won(amountWon > _value, amountWon);
+        emit Swapped("TOKA", _value);
     }
 
-    function playT2E(uint256 _value) payable public {
-        require(_value >= minBetT2E);
-        require(token.transferFrom(msg.sender, manager, _value));
+    function swapT2R(address _from, uint256 _value) payable public {
+        require(token.transferFrom(_from, manager, _value));
+        require(rupy.transferFrom(manager, _from, _value));
 
-        uint256 amountWon = _value * (50 + uint256(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - houseEdgeT2E) / 100 / E2TRatio;
-        require(msg.sender.send(amountWon));
+        emit Swapped("RUPY", _value);
+    }
 
-        emit Won(amountWon > (_value / E2TRatio), amountWon);
+    function playR2R(address _from, uint256 _value) payable public {
+        require(sR2R.bEnabled);
+        require(_value >= sR2R.minBet);
+        require(rupy.transferFrom(_from, manager, _value));
+
+        uint256 amountWon = _value * (50 + uint256(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - sR2R.houseEdge) / 100;
+        require(rupy.transferFrom(manager, _from, amountWon));
+
+        emit Won(amountWon > _value, "RUPY", amountWon);
+    }
+
+    function playR2E(address _from, uint256 _value) payable public {
+        require(sR2E.bEnabled);
+        require(_value >= sR2E.minBet);
+        require(rupy.transferFrom(_from, manager, _value));
+
+        uint256 amountWon = _value * (50 + uint256(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - sR2E.houseEdge) / 100 / E2R_Ratio;
+        require(_from.send(amountWon));
+
+        emit Won(amountWon > (_value / E2R_Ratio), "ETH", amountWon);
     }
 
     function playE2E() payable public {
-        require(msg.value >= minBetE2E);
+        require(sE2E.bEnabled);
+        require(msg.value >= sE2E.minBet);
 
-        uint amountWon = msg.value * (50 + uint(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - houseEdgeE2E) / 100;
+        uint amountWon = msg.value * (50 + uint(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - sE2E.houseEdge) / 100;
         require(msg.sender.send(amountWon));
 
-        emit Won(amountWon > msg.value, amountWon);
+        emit Won(amountWon > msg.value, "ETH", amountWon);
     }
 
-    function playE2T() payable public {
-        require(msg.value >= minBetE2T);
+    function playE2R() payable public {
+        require(sE2R.bEnabled);
+        require(msg.value >= sE2R.minBet);
 
-        uint amountWon = msg.value * (50 + uint(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - houseEdgeE2T) / 100 * E2TRatio;
-        require(token.transferFrom(manager, msg.sender, amountWon));
+        uint amountWon = msg.value * (50 + uint(keccak256(block.timestamp, block.difficulty, salt++)) % 100 - sE2R.houseEdge) / 100 * E2R_Ratio;
+        require(rupy.transferFrom(manager, msg.sender, amountWon));
 
-        emit Won(amountWon > (msg.value * E2TRatio), amountWon);
+        emit Won(amountWon > (msg.value * E2R_Ratio), "RUPY", amountWon);
     }
 
     // function for owner to check contract balance
@@ -210,5 +222,8 @@ contract LuckyBar is Bank {
     }
     function checkContractBalanceToka() onlyOwner public view returns(uint) {
         return token.balanceOf(manager);
+    }
+    function checkContractBalanceRupy() onlyOwner public view returns(uint) {
+        return rupy.balanceOf(manager);
     }
 }
