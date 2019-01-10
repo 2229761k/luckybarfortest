ed5932ecdbfd84bba84aeb68bf716b86fd1be8eb
Dohyung Kim <dohyungkim@Dohyungui-MacBook-Pro.local>
2019-01-10 11:52:37 +0900

    simple js fixing...

--- simpleserver/simple.js
+++ simpleserver/simple.js
@@ -2,15 +2,21 @@ const express = require('express');
 const logger = require('morgan');
 const helmet = require('helmet');
 const app = express();
+const ipfilter = require('express-ipfilter').IpFilter;
 
-const LocalStorage = require('node-localstorage').LocalStorage;
-localStorage = new LocalStorage('./log');
-
+// Whitelist the following IPs
+const ips = ['127.0.0.1'];
+ 
+app.use(ipfilter(ips, {mode: 'allow'}));
 app.use(express.json());
 app.use(logger("combined"));
 app.use(helmet());
 
+const LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./log');
+
 var index = 0
+var type = ["e2e", "e2c", "c2e", "c2c"]
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,33 +25,32 @@ app.all('/*', function(req, res, next) {
 });
 
 app
-  .route("/save/:date/:amount/:address")
+  .route("/save/:type/:date/:amount/:address")
   .get((req, res) => {
-    const {params: {date, amount, address}} = req;
-    var obj = {"Date" : date, "Amount" : amount, "Address":address}
+    const {params: {type, date, amount, address}} = req;
+    var obj = {"Type" : type, "Date" : date, "Amount" : amount, "Address":address}
     console.log("new item", obj);
 
     index = localStorage.getItem('index');
     if(index == null) index = 0;
 
-    localStorage.setItem(index.toString(), JSON.stringify(obj));
+    localStorage.setItem(type + index.toString(), JSON.stringify(obj));
     localStorage.setItem('index',++index);
     console.log(index);
+    console.log(type)
 
   })
-
 app
-  .route("/loadtotalresult")
+  .route("/loadtotalresult/:type")
   .get((req, res) => {
     var before = [];
-
     var index = localStorage.getItem('index');
     if(index == null) return;
 
     var limit = index - 100 > 0 ? index - 100 : 0;
 
     for(j=index-1; j>=limit; j--){
-        before.push(JSON.parse(localStorage.getItem(j)));
+        before.push(JSON.parse(localStorage.getItem(type+j)));
     }
 
     // console.log('index: ', index);
@@ -60,7 +65,7 @@ app
   })
 
   app
-  .route("/loadranking")
+  .route("/loadranking/")
   .get((req, res) => {
     var before = [];
     var after_sort = [];
@@ -72,7 +77,7 @@ app
 
     for(j=index-1; j>=limit; j--){
 
-        before.push(JSON.parse(localStorage.getItem(j)));
+        before.push(JSON.parse(localStorage.getItem(type+j)));
     }
 
     // console.log('index: ', index);
